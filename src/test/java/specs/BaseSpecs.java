64167d24b94ceb82f13b77a41600a4f08fb836a4
Olga Palushina <lokipypzemli@mail.ru>
2025-10-05 21:52:50 +0300

    add .filter in test

--- src/test/java/specs/BaseSpecs.java
+++ src/test/java/specs/BaseSpecs.java
@@ -7,15 +7,18 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.given;
+
 
 public class BaseSpecs {
 
-    public static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri(BaseConfigProvider.baseConfig.baseUri())
-            .setBasePath(BaseConfigProvider.baseConfig.basePath())
-            .setContentType("application/json")
-            .log(LogDetail.ALL)
-            .build();
+    public static RequestSpecification requestSpec = given()
+            .filter(withCustomTemplates())
+            .log().all()
+            .contentType("application/json")
+            .baseUri(BaseConfigProvider.baseConfig.baseUri())
+            .basePath(BaseConfigProvider.baseConfig.basePath());
 
     public static ResponseSpecification responseSpec(int statusCode) {
         return new ResponseSpecBuilder()
